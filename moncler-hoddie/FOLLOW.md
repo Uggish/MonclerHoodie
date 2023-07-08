9cdf779e9b1469966d76be8002bfc809a5a5dda5
Uggish Resources <104546122+Uggish@users.noreply.github.com>
2023-07-08 04:29:59 +0200

    Add files via upload
    Color? Black & Red.
    
    Good Quality
    
    FiveM Ready
    
    Redesign approved
    
    Discord:
    
    https://discord.gg/dDnQanU3

---
+++ moncler-hoddie/FOLLOW.md
@@ -0,0 +1,7 @@
+* You must not try to spread this further. 
+* You may not redesign it, add any detail whatsoever, etc.
+
+Support:
+realuggish#4382
+https://discord.gg/MPau5YNf
+
---
+++ moncler-hoddie/fxmanifest.lua
@@ -0,0 +1,14 @@
+lua54 'yes'
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Uggishhh#4551'
+description 'Uggish Resource Mekonom Byxor - Uggish.'
+version '1.5'
+
+
+
+-- Discord https://discord.gg/ARqSTWMf
+-- Tebex https://uggish-store.tebex.io/
+-- Discord Account Uggishhh#4551
+-- /// UGGISH
